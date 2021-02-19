9e2d00349b63d329f89c05a98b25486df559d22f
PLACKNATION <mannyflory27@gmail.com>
2021-02-19 12:07:35 -0500

    added nav bar and fixed some positioning

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './views/App';
+import NavigationBar from './views/NavigationBar'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <NavigationBar />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
